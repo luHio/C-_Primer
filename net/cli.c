34442224f37608b2f94d1ceeca7be01328f5f294
luHio <atbisr@163.com>
2020-06-09 15:30:19 +0800

    error1

--- net/cli.c
+++ net/cli.c
@@ -22,7 +22,7 @@ int main(int argc,char *argv[]){
 
 	bzero(&server_addr,sizeof(server_addr));
 	server_addr.sin_family=AF_INET;
-	server_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+	server_addr.sin_addr.s_addr=htonl("127.0.0.1");
 	server_addr.sin_port=htons(PORT);
 
 	connect(s,(struct sockaddr*)&server_addr,sizeof(struct sockaddr));
