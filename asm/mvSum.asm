55df9b6793dbaed106c8fce29e347473569e42fd
luHio <atbisr@163.com>
2020-06-12 17:28:01 +0800

    rename

--- asm/mvSum.asm
+++ asm/mvSum.asm
@@ -1,19 +1,18 @@
 assume cs:code
 
 code segment
-	start:	mov ax,0ffffh
-		mov ds,ax
+	start:	
 		mov bx,0h
 		mov cx,10h  ;设置ds的值和循环次数
 		
-	addSum:	mov dl,ds:[bx] ;用一个寄存器临时保存ds中的值
+	addSum:	mov ax,0ffffh
+		mov ds,ax
+		mov dl,ds:[bx] ;用一个寄存器临时保存ds中的值
 		mov ax,20h
 		mov ds,ax  ;临时修改ds的值
 		mov ds:[bx],dl   ;将寄存器中的值放在修改后的ds指向的地址中
 		inc bx ;bx++ 
-		mov ax,0ffffh   
-		mov ds,ax ;将ds的值改回去
-		loop addSum   //循环
+		loop addSum   ;循环
 
 
 		mov ax,4c00h
